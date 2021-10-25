fc09760524fc63737a5ad4807a701a98625847cc
WeiselO <90724297+WeiselO@users.noreply.github.com>
2021-10-25 15:20:52 +0200

    Hardcoded cities into main main print function

--- Heaps/src/com/company/Main.java
+++ Heaps/src/com/company/Main.java
@@ -4,17 +4,36 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MinHeap<String> myMinHeap=new MinHeap<>();
-        myMinHeap.insert("Albert");
-        myMinHeap.insert("Line");
-        myMinHeap.insert("Benjamin");
-        myMinHeap.insert("Aisha");
-        myMinHeap.insert("Anya");
-        myMinHeap.insert("Patrick");
-
-        while (!myMinHeap.isEmpty()){
-            System.out.println(myMinHeap.extractMin());
-        }
+        AdjGraph MyGraph=new AdjGraph();
+        Vertex Eskildstrup = new Vertex("Eskildstrup");
+        Vertex Maribo = new Vertex("Maribo");
+        Vertex NykøbingF = new Vertex( "NykøbingF");
+        Vertex Vordingborg = new Vertex("Vordingborg");
+        Vertex Haslev = new Vertex("Haslev");
+        Vertex Korsør = new Vertex( "Korsør");
+        Vertex Køge = new Vertex("Køge");
+        MyGraph.addVertex(Eskildstrup);
+        MyGraph.addVertex(Maribo);
+        MyGraph.addVertex(NykøbingF);
+        MyGraph.addVertex(Vordingborg);
+        MyGraph.addVertex(Haslev);
+        MyGraph.addVertex(Korsør);
+        MyGraph.addVertex(Køge);
+        MyGraph.addEdge(Eskildstrup,Maribo,28);
+        MyGraph.addEdge(Eskildstrup,NykøbingF, 13);
+        MyGraph.addEdge(Eskildstrup,Vordingborg,25);
+        MyGraph.addEdge(Haslev,Korsør,60);
+        MyGraph.addEdge(Haslev,Køge,24);
+        MyGraph.addEdge(Haslev,Vordingborg,1);
+        MyGraph.addEdge(Haslev,Haslev,1);
+        MyGraph.addEdge(Haslev,Køge, 1);
+        MyGraph.addEdge(Haslev,NykøbingF, 1);
+        MyGraph.addEdge(Haslev,Vordingborg,3);
+        MyGraph.addEdge(Haslev,Korsør, 3);
+        MyGraph.addEdge(Haslev,Køge,4);
+        MyGraph.addEdge(Haslev,Køge, 1);
+        MyGraph.PrintGraph();
+        MyGraph.MSTPrims();
     }
 
 }
