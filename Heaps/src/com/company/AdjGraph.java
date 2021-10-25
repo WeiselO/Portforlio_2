f72592958b1be8bb1d8503e0a8af547e5917feee
Anne-Thea <annethearendtorff@gmail.com>
2021-10-25 15:15:07 +0200

    - Got MST prims working! Yay!

--- Heaps/src/com/company/AdjGraph.java
+++ Heaps/src/com/company/AdjGraph.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AdjGraph {
     ArrayList<Vertex> vertices;
@@ -15,12 +16,12 @@ public class AdjGraph {
             System.out.println("Vertex not in graph");
             return;
         }
-
-        Edge e=new Edge(from, to,weight);
+        Edge e =new Edge(from, to, weight);
+        Edge e2=new Edge(to, from, weight);
     }
+
     public  void PrintGraph(){
-        for (int i=0;i<vertices.size();i++)
-        {
+        for (int i=0;i<vertices.size();i++){
             System.out.println(" From Vertex: "+ vertices.get(i).name);
             Vertex currentfrom=vertices.get(i);
             for(int j=0; j<currentfrom.OutEdges.size();j++){
@@ -30,12 +31,47 @@ public class AdjGraph {
             System.out.println(" ");
         }
     }
+
+    public void MSTPrims() {
+        MinHeap<Vertex> Q = new MinHeap<>();
+        for (int vertex = 0; vertex < this.vertices.size(); vertex++) {
+            vertices.get(vertex).setDist(Integer.MAX_VALUE);
+            vertices.get(vertex).setPrev(null);
+            vertices.get(vertex).visited = false;
+            Q.insert(vertices.get(vertex));
+        }
+        if (vertices.size() > 0){
+            int start = 0;
+            vertices.get(start).setDist(0); //starting at 0.
+            int position = Q.getPosition(vertices.get(start)); //getting the position of the starting vertex
+            Q.decreaseKey(position); //Fixing the minheap since we changed the value of the starting vertex.
+            int MSTtotal = 0;
+            while (!Q.isEmpty()){
+                Vertex u = Q.extractMin(); //Taking the smallest u.
+                for (int uOutEdgeIndex = 0; uOutEdgeIndex < u.OutEdges.size(); uOutEdgeIndex++) {
+                    Edge uOutEdge = u.OutEdges.get(uOutEdgeIndex);
+                    if (uOutEdge.weight < uOutEdge.to.dist && u.visited == false){
+                        uOutEdge.to.setDist(uOutEdge.weight);
+                        uOutEdge.to.setPrev(u);
+                        int pos = Q.getPosition(uOutEdge.to);
+                        Q.decreaseKey(pos);
+                    }
+                }
+                System.out.println(u.dist);
+                MSTtotal += u.dist;
+                u.visited = true;
+            }
+            System.out.println("MST size is: "+MSTtotal);
+        }
+    }
 }
 
 class Vertex implements Comparable<Vertex>{
     String name;
     ArrayList<Edge> OutEdges;
     Integer dist= Integer.MAX_VALUE;
+    Vertex prev= null;
+    boolean visited = false;
     public Vertex(String id){
         name=id;
         OutEdges=new ArrayList<Edge>();
@@ -43,6 +79,7 @@ class Vertex implements Comparable<Vertex>{
     public void addOutEdge(Edge e) {
         OutEdges.add(e);
     }
+
 
     @Override
     public int compareTo(Vertex o) {
@@ -52,6 +89,35 @@ class Vertex implements Comparable<Vertex>{
             return 1;
         return 0;
     }
+
+    public Integer getDist() {
+        return dist;
+    }
+
+    public void setDist(Integer dist) {
+        this.dist = dist;
+    }
+
+    public Vertex getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Vertex prev) {
+        this.prev = prev;
+    }
+}
+
+class Edge{
+    Integer weight;
+    Vertex from;
+    Vertex to;
+    public Edge(Vertex from, Vertex to, Integer cost){
+        this.from=from;
+        this.to=to;
+        this.weight=cost;
+        this.from.addOutEdge(this);
+    }
+
 
     public static void main(String[] args) {
         AdjGraph MyGraph=new AdjGraph();
@@ -83,19 +149,7 @@ class Vertex implements Comparable<Vertex>{
         MyGraph.addEdge(e,g,4);
         MyGraph.addEdge(f,g, 1);
         MyGraph.PrintGraph();
-    }
-
-}
-
-class Edge{
-    Integer weight;
-    Vertex from;
-    Vertex to;
-    public Edge(Vertex from, Vertex to, Integer cost){
-        this.from=from;
-        this.to=to;
-        this.weight=cost;
-        this.from.addOutEdge(this);
+        MyGraph.MSTPrims();
     }
 }
 
