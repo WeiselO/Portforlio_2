4cae6e8780cd03771ffe8f04bb3a1a2fb0cca5bb
Anne-Thea <annethearendtorff@gmail.com>
2021-10-26 11:15:26 +0200

    - Deleted unused classes - Added vertex to separate class

--- Heaps/src/com/company/AdjGraph.java
+++ Heaps/src/com/company/AdjGraph.java
@@ -68,37 +68,6 @@ public class AdjGraph {
     }
 }
 
-class Vertex implements Comparable<Vertex>{
-    String name;
-    ArrayList<Edge> OutEdges;
-    Integer dist= Integer.MAX_VALUE;
-    Vertex prev= null;
-    boolean visited = false;
-    public Vertex(String id){
-        name=id;
-        OutEdges=new ArrayList<Edge>();
-    }
-    public void addOutEdge(Edge e) {
-        OutEdges.add(e);
-    }
-
-
-    @Override
-    public int compareTo(Vertex o) {
-        if (this.dist<o.dist)
-            return -1;
-        if (this.dist>o.dist)
-            return 1;
-        return 0;
-    }
-    public void setDist(Integer dist) {
-        this.dist = dist;
-    }
-
-    public void setPrev(Vertex prev) {
-        this.prev = prev;
-    }
-}
 class Edge{
     Integer weight;
     Vertex from;
